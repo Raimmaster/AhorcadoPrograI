0208a2130649542c3c2857760e340e670484cf35
Raim Interiano <rai.interiano@gmail.com>
2014-08-21 16:10:31 -0600

    Avances - Estadísticas
    Variables, arreglos e instrucciones para las estadísticas están
    ingresados.

--- Ahorcado.java
+++ Ahorcado.java
@@ -13,9 +13,7 @@ public class Ahorcado{
 				"Falacia", "Acelerado", "Ninja", "Caballero", "Mensaje"};
 
 		String dificultad = "NORMAL"; 
-		int opcion, ganados = 0, perdidos = 0, jugados = 0; //variables para opcion y para juegos
-		int adivinados = 0, fallados = 0, winByChar = 0, winByString = 0; //variables para caracters adivinados, fallados
-		//y juegos ganados por caracteres o por cadenas
+		int opcion, ganados = 0, perdidos = 0, jugados = 0; //variables para opcion y para juegos		
 		double avgWin, avgLose; //promedios para ganes y perdidas
 
 		char [] abecedario = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
@@ -32,6 +30,15 @@ public class Ahorcado{
 				"\n5 - Salir");
 
 				opcion = lea.nextInt();
+
+				/*Creación de variables para controlar los ganes
+				y perdidas dentro de la sesión de juegos*/
+				int ganesSesion = 0, perdidasSesion = 0;
+
+				int adivinados = 0, fallados = 0, loseByString = 0, winByString = 0; //variables para caracters adivinados, fallados
+				//y juegos ganados o perdidos ganados por cadenas
+				char caracterPop = 0; //el caracter más veces ingresados en la sesión
+				int [] abcSesion = new int [26]; //contendra el abecedario 
 
 				switch (opcion){
 					case 1:
@@ -77,13 +84,16 @@ public class Ahorcado{
 										contador = 0;
 										System.out.print("\nIngrese caracter: "); 
 										caracter = lea.next().charAt(0);
-										
+										abcSesion[caracter - 97]++;
+
 										//Encontrando coincidencias
 										boolean acierto = false; //variable de control por si encuentra o no en este intento
 										while (contador < longitud){
 											if (Character.toLowerCase(palabras[palabrax].charAt(contador)) == Character.toLowerCase(caracter)){
 												encontrados[contador] = caracter;
 												acertados++;
+												adivinados++;										
+												
 												acierto = true;
 											}
 											contador++;
@@ -92,17 +102,18 @@ public class Ahorcado{
 										if (acierto == false){
 											System.out.println("\nEse caracter no existe en la palabra.");
 											restantes--;
+											fallados++;
 										}else
 											System.out.println("\nCaracter adivinado!");
 
 										//Guardando registros de caracteres mas ingresados
 										
 										//contador = 1;
-										for (contador = 0; contador < 26; contador++){
-											if ((caracter == contador + 100) || (caracter == contador + 140)){
-												abecedario[contador]++;
-											}											
-										}
+										// for (contador = 0; contador < 26; contador++){
+										// 	if ((caracter == contador + 100) || (caracter == contador + 140)){
+										// 		abecedario[contador]++;
+										// 	}											
+										// }
 
 										//System.out.println(contador); **PRUEBA
 
@@ -113,13 +124,14 @@ public class Ahorcado{
 
 										//Validando si palabra armada por caracter y si restan intentos, Registrando juegos ganados y perdidos
 										if (acertados == longitud){
-											ganados++;
-											winByChar++;
+											//ganados++;
+											ganesSesion++;
 											restantes = 0;
 
 											System.out.print("\nGanaste! Palabra Armada!");
 										} else if(restantes == 0){
-											perdidos++;
+											//perdidos++;
+											perdidasSesion++;
 
 											System.out.print("\nNo te quedan mas intentos! Has perdido, intentalo de nuevo!");
 										}
@@ -129,12 +141,16 @@ public class Ahorcado{
 										cadena = lea.next();
 										//Validando si palabra ingresada es correcta
 										if (cadena.equalsIgnoreCase(palabras[palabrax])){
-											ganados++;
+											//ganados++;
+											ganesSesion++;
 											winByString++;
 
 											System.out.print("\nGanaste! Palabra ingresada correcta!");
 										}else{
-											perdidos++;
+											//perdidos++;
+											loseByString++;
+											perdidasSesion++;
+
 											System.out.print("\nPerdiste! Palabra ingresado incorrecta!");
 										}
 
@@ -149,7 +165,32 @@ public class Ahorcado{
 							opjugar = lea.next();
 						}while (opjugar.equalsIgnoreCase("si"));		
 
+						//Incrementaciones de ganes, perdidas y juegos
 						jugados++;
+						ganados += ganesSesion;
+						perdidos += perdidasSesion;
+
+						int repeat = abcSesion[0];
+
+						for (int i = 1; i <= 25; i++){
+							repeat = Math.max(repeat, abcSesion[i]);
+						}
+
+						for (int i = 0; i <= 25; i++){
+							if (repeat == abcSesion[i]){
+								caracterPop = (char)(i + 97);
+								break;
+							}
+						}
+
+						System.out.println("***DETALLE ESTADISTICO DE SESION DE JUEGOS***" + 
+							"\nCaracteres adivinados: " + adivinados + 
+							"\nCaracteres fallados: " + fallados + 
+							"\nJuegos ganados: " + ganesSesion + 
+							"\nJuegos ganados al ingresar la palabra: " + winByString + 
+							"\nJuegos perdidos al ingresar la palabra: " + loseByString +
+							"\nCaracter mas veces ingresado: " + caracterPop);
+
 						break;
 					case 2:
 							//Variable para condicionar si desea agregar las 20 nuevas palabras o no
@@ -189,8 +230,12 @@ public class Ahorcado{
 						}
 						break;
 					case 4:
-						avgWin = (double)jugados / (double)ganados;
-						avgLose = (double)jugados / (double)perdidos;						
+						int totalJuegos = ganados + perdidos;
+						avgWin = (double)totalJuegos / (double)ganados;
+						avgLose = (double)totalJuegos / (double)perdidos;	
+
+
+
 						char popular = 'a';
 						System.out.println("***REPORTE ESTADISTICO***\nCantidad de veces que se eligio la opcion JUGAR: " + jugados +
 							"\nJuegos ganados: " + ganados + "\nPromedio de juegos ganados: " + avgWin +
